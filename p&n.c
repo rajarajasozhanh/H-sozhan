ef01c5da9333cedb2c75037ee152265abb100fbf
rajarajasozhanh <35727808+rajarajasozhanh@users.noreply.github.com>
2018-01-25 11:08:38 +0000

    Create p&n.c

---
+++ p&n.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+void main()
+{
+int n;
+scanf("%d,&n");
+{
+if(n=0)
+printf("the value is zero");
+else if(n<0)
+printf("the value is negative");
+else
+printf("the value is positive");
+}
+}
